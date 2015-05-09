27d7ce05997997a221af40629cb1788dec18cdfd
angelomarsanz <angelomarsanz@gmail.com>
2015-05-09 10:42:41 -0430

    Version 09-05-2015 - 1

---
+++ ejercicio6.php
@@ -0,0 +1,7 @@
+<?php
+	echo nl2br("Esto es una prueba\n") ; // Esto es un comentario
+	/* Esta es otra línea de comentario
+	   que continúa aquí
+	   y finaliza aquí. Recuerda que el usuario no ve los comentarios, solo el programador */
+	echo 'Esto es otra prueba'; # Esta es otra manera de hacer comentarios
+?>
